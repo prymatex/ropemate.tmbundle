3dff684998a00f90fd226949905a45e5fe273953
diegomvh <diegomvh@gmail.com>
2012-06-03 13:12:46 -0300

    new dialgo system

--- Support/guis/input/__init__.py
+++ Support/guis/input/__init__.py
@@ -2,7 +2,24 @@
 #-*- encoding: utf-8 -*-
 
 from PyQt4 import QtGui
+from prymatex.core.plugin.dialog import PMXBaseDialog
 
-def load(application, settings):
-    text, accepted = QtGui.QInputDialog.getText(application.mainWindow, "New Name", settings.title, text = settings.result)
-    return text
+class InputDialog(QtGui.QInputDialog, PMXBaseDialog):
+    def __init__(self, parent = None):
+        QtGui.QInputDialog.__init__(self, parent)
+        PMXBaseDialog.__init__(self)
+    
+    def setParameters(self, parameters):
+        if "title" in parameters:
+            self.setWindowTitle(parameters["title"])
+        if "result" in parameters:
+            self.setTextValue(parameters["result"])
+        self.setLabelText("New name")
+        
+    def execModal(self):
+        code = self.exec_()
+        if code == QtGui.QDialog.Accepted:
+            return self.textValue()
+        return ""
+        
+dialogClass = InputDialog
